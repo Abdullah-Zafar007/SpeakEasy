7737468c08de779d9fca7f63d50ffcd03961873e
Abdullah-Zafar007 <161499653+Abdullah-Zafar007@users.noreply.github.com>
2025-06-27 23:51:11 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,19 @@
+
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/speech_therapy", {
-    
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
     });
-    console.log("MongoDB connected");
+    console.log("✅ MongoDB connected");
   } catch (error) {
-    console.error("MongoDB connection error:", error);
+    console.error("❌ MongoDB connection error:", error);
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
